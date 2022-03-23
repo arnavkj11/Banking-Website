de6972f2bec0b9a87e175ca2d7dd719103ebcb76
arnavkj11 <arnavkj11@gmail.com>
2022-03-23 14:45:58 +0530

    my first commit

---
+++ footer.php
@@ -0,0 +1,51 @@
+<link rel="stylesheet" type="text/css" href="css/footer.css">
+<footer>
+<div class="container">
+<div class="follow_us">
+    <label>Follow Us</label>
+    <ul>
+        <li><a href="https://www.facebook.com/">Facebook</a></li>
+        <li><a href="https://twitter.com/">Twitter</a></li>
+        <li><a href="https://www.instagram.com/">Instagram</a></li>
+    
+    </ul>
+    </div>
+    <div class="contact" id="contactus">
+        <label>Contact Us</label>
+        <ul>
+            <p>302 BOY's Hostel,</p><p>MPTP Shirpur</p>
+            <p>Tel : 8209938189</p>
+            <p> Email: contact@bankofengineer.com</p>
+            
+        </ul>
+    </div>
+        <div class="links">
+            <label>Important Links</label>
+            <ul>    
+            <li><a href="index.php#aboutus" target="_blank">About Us</a></li>
+            <!-- <li><a href="">Loan</a></li>
+            <li><a href="">Calculator</a></li>  -->
+            </ul><br>
+    </div>
+
+    <div class="links">
+            <label>Important Links</label>
+            <ul>    
+            <li><a href="index.php#aboutus" target="_blank">Link 1</a></li>
+            <li><a href="">Link 2</a></li>
+            <li><a href="">Link 3</a></li> 
+            </ul><br>
+    </div>
+    
+    
+    </div>
+    <div class="copyright">
+        <span>Made By: Arnav Kumar Jain</span>
+    </div>
+    
+    <div class="bestview">
+    <span></span>
+    
+    </div>
+
+</footer>
---
+++ header.php
@@ -0,0 +1,46 @@
+<link rel="stylesheet" type="text/css" href="css/header.css">
+        <div class="header_main">
+		<div class="navBar">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="index.php#aboutus">About Us</a></li>
+                <li><a href="index.php#contactus">Contact Us</a></li>
+				<li><a href="staff_login.php">Staff Login</a></li>
+			
+			</ul>
+		</div>
+	 <a href="index.php">
+		<div class="logo-name">
+			<div class="logo">
+             <img class="logo_img" src="img/bankicon.jpg">
+			</div>
+			
+			<div class="name">
+				<h5> Bank Of Engineers</h5></a><br>
+				<h6>		Online Banking System</h6>
+			</div>
+        </div>
+            
+            
+        <div class="dif_banking">
+			<div class="retail_banking">
+				<a href="customer_reg_form.php">  Open Account   </a>
+			</div>
+			<div class="corporate_banking">
+				<a href="#">   Internet Banking   </a>
+			</div>
+			<div class="international_banking">
+				<a href="fund_transfer.php">   Fund Transfer   </a>
+			</div>
+			<div class="international_banking">
+				<a href="debit_card_form.php">   Apply Debit Card   </a>
+			</div>
+                
+            <div class="bank_servic">
+				<a href="#">Services</a>
+			</div>
+		</div>
+            
+           
+			
+	</div>
